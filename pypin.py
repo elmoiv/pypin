7b632620613bb64840f63992da86d95f04d64bdb
Khaled ElMorshedy <elmoiv@yahoo.com>
2020-10-02 17:31:06 +0200

    Add files via upload

--- pypin.py
+++ pypin.py
@@ -1,23 +1,29 @@
+import os
 from downloader import download
 from tools import *
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 __import__('warnings').filterwarnings("ignore")
 
+os.makedirs('Pypin', exist_ok=1)
+os.chdir('Pypin')
+
 TYPES = [
-    ('Video', 'mp4'),
-    ('Image', 'jpg'),
-    ('GIF',   'gif')
+    'Video',
+    'GIF',
+    'Image',
     ]
 
 ID, HTML           =      getHtml(input('URL: '))
 JSON               =      getJson(HTML)
 _type, mediaUrl    =      decideType(JSON)
+EXT                =      mediaUrl.split('.')[-1]
 
 if _type == None:
     print('Nothing Found!')
     exit(0)
 
-file_name = f'{ID}.{TYPES[_type][1]}'
+file_name = f'{ID}.{EXT}'
 
-print(f'Downloading {TYPES[_type][0]}: {file_name}')
+print(f'Downloading {TYPES[_type]}: {file_name}')
 download(mediaUrl, file_name)
